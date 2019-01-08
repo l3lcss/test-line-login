16fe86a75f10da765050cfba14d2610f2c47e0ad
princel3lcS <princeboy__@hotmail.com>
2019-01-08 17:12:08 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <!-- <img src="../assets/btn_login_base.png" alt=""> -->
+    <div class="btn-login-line" @click="login()"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    async login () {
+      const options = {
+        method: 'GET',
+        url: 'https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=1635880494&redirect_uri=http://localhost:8080/&state=123abc'
+      }
+      let data = await axios(options)
+      console.log(data, 'data')
+    }
+  }
+}
+</script>
+
+<style>
+.btn-login-line {
+  background-image: url('../assets/btn_login_base.png');
+  width: 303px;
+  height: 88px;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+.btn-login-line:hover {
+  background-image: url('../assets/btn_login_hover.png');
+  width: 303px;
+  height: 88px;
+  cursor: pointer;
+}
+.btn-login-line:active {
+  background-image: url('../assets/btn_login_press.png');
+  width: 303px;
+  height: 88px;
+  cursor: pointer;
+}
+</style>
